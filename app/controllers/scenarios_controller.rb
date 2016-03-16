fb9f976ec8ca1f315e2fd4dd7ebd079200b87406
Mic <mic.cassano@gmail.com>
2016-03-16 12:41:15 +0000

    Modify scenarios controller to serve up scenarios JSON

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -1,4 +1,13 @@
 class ScenariosController < ApplicationController
+  before_action :load_scenarios
+
   def index
+    render json: @scenarios
+  end
+
+  private
+
+  def load_scenarios
+    @scenarios = Scenario.all
   end
 end
