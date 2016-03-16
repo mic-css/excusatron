30c72836cfb2c581c9962471cefe274643d62f15
Henry Gordon <hwgordon247@gmail.com>
2016-03-16 14:53:47 +0000

    there is the basic functionality of the overall app, but not currently importing from a json file

--- spec/javascripts/protractor_specs/e2e/homePageFeaturesSpec.js
+++ spec/javascripts/protractor_specs/e2e/homePageFeaturesSpec.js
@@ -5,14 +5,35 @@ describe('Alibot', function(){
 
   });
 
+  it('should have a title', function() {
+   expect(browser.getTitle()).toEqual('Alibot');
+ });
+
 
   it("Displays a heading", function(){
-    expect(element(by.id('application')).getText('Alibot')).toContain('Alibot');
+    expect(element(by.id('application')).getText()).toContain('Alibot');
 
-    // expect(element(by.id('alibot')).getTitle()).toEqual('Alibot');
   });
 
-  // it('Displays a list of scenarios', function(){
-  //   expect(element(by.model('alibotCtrl.scenarios'))).toEqual('Feeling Flakey');
-  // });
+  it("has a scenarion button", function(){
+    expect(element(by.id('application')).getText()).toContain('Running Late');
+
+  });
+
+  it("can select the scenario 'Running late'", function(){
+    element(by.id('runningLate')).click();
+    expect(element(by.id('application')).getText()).toContain('Running Late Excuse:')
+  });
+
+  it("can return to the scenario page", function(){
+    element(by.id('runningLate')).click();
+    element(by.id('return')).click();
+    expect(element(by.id('application')).getText()).not.toContain('Running late Excuse:')
+  });
+
+  it("can select the scenario 'Can\'t Go'", function(){
+    element(by.id('cantGo')).click();
+    expect(element(by.id('application')).getText()).toContain('Can\'t Go')
+  });
+
 });
