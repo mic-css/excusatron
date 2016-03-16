677081a79c38bbbd7a6a2b4e55858ef351aaa6ec
Henry Gordon <hwgordon247@gmail.com>
2016-03-16 12:30:49 +0000

    ready to create tests.. woohoo

--- spec/javascripts/protractor_specs/e2e/homePageFeaturesSpec.js
+++ spec/javascripts/protractor_specs/e2e/homePageFeaturesSpec.js
@@ -1,6 +1,21 @@
 describe('Alibot', function(){
-  it('Displays a list of scenarios', function(){
-    browser.get('http://localhost:8080');
-    expect(element(by.model('alibotCtrl.scenarios')).toEqual('Feeling Flakey'));
+
+  beforeEach(function(){
+    browser.get('http://localhost:3000');
+    
   });
+
+  it('should have a title', function() {
+   expect(browser.getTitle()).toEqual('Alibot');
+ });
+
+  it("Displays a heading", function(){
+    element(by.id('application')).getText('Alibot');
+
+    // expect(element(by.id('alibot')).getTitle()).toEqual('Alibot');
+  });
+
+  // it('Displays a list of scenarios', function(){
+  //   expect(element(by.model('alibotCtrl.scenarios'))).toEqual('Feeling Flakey');
+  // });
 });
