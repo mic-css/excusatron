cbd9a2180544e7bd31e9588a01e99cf3ed4e920a
Henry Gordon <hwgordon247@gmail.com>
2016-03-16 13:19:27 +0000

    have a passing test for heading

--- spec/javascripts/protractor_specs/e2e/homePageFeaturesSpec.js
+++ spec/javascripts/protractor_specs/e2e/homePageFeaturesSpec.js
@@ -2,15 +2,12 @@ describe('Alibot', function(){
 
   beforeEach(function(){
     browser.get('http://localhost:3000');
-    
+
   });
 
-  it('should have a title', function() {
-   expect(browser.getTitle()).toEqual('Alibot');
- });
 
   it("Displays a heading", function(){
-    element(by.id('application')).getText('Alibot');
+    expect(element(by.id('application')).getText('Alibot')).toContain('Alibot');
 
     // expect(element(by.id('alibot')).getTitle()).toEqual('Alibot');
   });
