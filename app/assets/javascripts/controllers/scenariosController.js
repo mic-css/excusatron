71696a52ac3eaed671a6082566ea75d2765223c0
Henry Gordon <hwgordon247@gmail.com>
2016-03-17 14:28:43 +0000

    can now call the backend api and access the info in the front end

--- app/assets/javascripts/controllers/scenariosController.js
+++ app/assets/javascripts/controllers/scenariosController.js
@@ -3,10 +3,19 @@
 
   angular
     .module('alibot')
-    .controller("scenariosController", [function(){
+    .controller("scenariosController", [ '$http', function($http){
     var self = this;
+    self.api = [];
+    self.test = 'hello';
 
-    this.test = 'hello';
+    $http({
+      method: 'GET',
+      url: 'http://localhost:3000/scenarios'
+    }).then(function successCallback(response) {
+      self.api = response.data;
+    });
+
+
 
   }]);
 }());
