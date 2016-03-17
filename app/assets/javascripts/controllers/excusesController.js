160ef7d76682742763f2016918864244146e288a
Henry Gordon <hwgordon247@gmail.com>
2016-03-17 17:56:46 +0000

    can now access relevant excuses from the backend and display them in angular. successful TDD!

--- app/assets/javascripts/controllers/excusesController.js
+++ app/assets/javascripts/controllers/excusesController.js
@@ -5,14 +5,15 @@
     .module('alibot')
     .controller("excusesController", ['$http', 'scenarioService', function($http, scenarioService) {
       var self = this;
-      self.test2 = scenarioService.scenarioId;
+      self.excuses = [];
+      
 
-      // $http({
-      //   method: 'GET',
-      //   url: 'http://localhost:3000/scenarios/' + scenarioId + '/excuses'
-      // }).then(function successCallback(response) {
-      //   self.scenarios = response.data;
-      // });
+      $http({
+        method: 'GET',
+        url: 'http://localhost:3000/scenarios/' + scenarioService.scenarioId + '/excuses'
+      }).then(function successCallback(response) {
+        self.excuses = response.data;
+      });
 
     }]);
 }());
