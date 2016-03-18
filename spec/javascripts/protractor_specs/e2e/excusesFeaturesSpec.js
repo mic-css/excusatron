1f895349dfd7de414fbe884c216f443de5acc524
Mic <mic.cassano@gmail.com>
2016-03-18 13:22:15 +0000

    Added styling

--- spec/javascripts/protractor_specs/e2e/excusesFeaturesSpec.js
+++ spec/javascripts/protractor_specs/e2e/excusesFeaturesSpec.js
@@ -2,13 +2,13 @@ describe('Excuses page', function () {
 
   beforeEach(function(){
     browser.get('http://localhost:3000');
-    element(by.linkText('Running late')).click();
+    element(by.linkText('RUNNING LATE')).click();
   });
 
   var excuses = element.all(by.css('.excuse'));
 
   it("has the relevant excuses to a given scenario", function() {
-    expect(excuses.first().getText()).toBe('Stuck in traffic');
+    expect(excuses.first().getText()).toBe('STUCK IN TRAFFIC');
     expect(excuses.count()).toBe(3);
   });
 
